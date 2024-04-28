9974e03a71e0892eb0a4d97ecc058d577736e4b8
Nagendra Gubbala <nagendragubbala999+9@gmail.com>
2024-04-28 12:36:07 +0530

    add bounce effect

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const Nav = () => {
     <>
       <nav className="flex bg-black w-screen justify-between pr-12 border-b-[1px]">
 
-        <ul className="flex w-2/5 justify-evenly py-3 px-2 text-white">
+        <ul className="flex w-2/5 justify-evenly px-2 text-white items-center">
           <li><a href="http://127.0.0.1:3000" className='no-underline'>Home</a></li>
           <li><a href="http://127.0.0.1:3000" className='no-underline'>About</a></li>
           <li><a href="http://127.0.0.1:3000" className='no-underline'>Contact</a></li>
