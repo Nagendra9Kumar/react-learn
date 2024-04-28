ed81aea57c876ef2b3acbdb2fb6c15befc19180f
Nagendra Gubbala <nagendragubbala999+9@gmail.com>
2024-04-28 13:39:59 +0530

    added hover green css

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App className="overflow-x-hidden h-screen" />
+    <App className="overflow-x-hidden h-screen fixed" />
   </React.StrictMode>
 );
 
