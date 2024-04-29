924e44d7fde4211538d0c79db7e5755a1af8f1d2
Nagendra Gubbala <nagendragubbala999+9@gmail.com>
2024-04-29 11:15:59 +0530

    changes are made at navbar and optimized for small screens

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import image from './image.png';
 const Nav = () => {
   return (
     <>
-      <nav className="flex bg-black w-screen h-[9vh]  justify-between items-center">
+      <nav className="flex bg-black w-screen h-[8vh]  justify-between items-center">
         <ul className="flex justify-start gap-7 max-sm:gap-2 max-sm:ml-2 mr-auto ml-7 px-2 max-sm:text-sm text-white text-xl ">
           <li><a href="http://127.0.0.1:3000" className='no-underline  hover:text-gray-500'>Home</a></li>
           <li><a href="http://127.0.0.1:3000" className='no-underline  hover:text-gray-500'>About</a></li>
@@ -19,7 +19,7 @@ const Nav = () => {
 const Sec = (props) => {
   return (
     <>
-      <section className='flex h-[91vh] bg-black justify-center items-center snap-start'>
+      <section className='flex h-[92vh] bg-black justify-center items-center snap-start'>
         <h1 className='text-white text-6xl max-md:text-3xl max-sm:text-xl animate-ping hover:text-green-500'>{props.uname}</h1>
       </section>
     </>
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <>
       <Nav />
-      <div className="App bg-black h-[91vh] snap-mandatory snap-y overflow-y-scroll overscroll-x-none">
+      <div className="App bg-black h-[92vh] snap-mandatory snap-y overflow-y-scroll overscroll-x-none">
         <Sec uname='Im Nagendra' />
         <Sec uname='Studing 2nd Cad' />
         <Sec uname='20 yr old' />
