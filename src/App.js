acd1e480e4c2c726ff0c3e96478f0619edff0769
Nagendra Gubbala <nagendragubbala999+9@gmail.com>
2024-04-29 10:53:40 +0530

    changes are made at navbar and optimized for small screens

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import image from './image.png';
 const Nav = () => {
   return (
     <>
-      <nav className="flex bg-black w-screen h-[9vh] justify-between pr-12 items-center">
-        <ul className="flex justify-start gap-7 mr-auto ml-7 px-2 text-white text-xl ">
-          <li><a href="http://127.0.0.1:3000" className='no-underline hover:text-gray-500'>Home</a></li>
-          <li><a href="http://127.0.0.1:3000" className='no-underline hover:text-gray-500'>About</a></li>
-          <li><a href="http://127.0.0.1:3000" className='no-underline hover:text-gray-500'>Contact</a></li>
-          <li><a href="http://127.0.0.1:3000" className='no-underline hover:text-gray-500'>Portfolio</a></li>
+      <nav className="flex bg-black w-screen h-[9vh]  justify-between pr-12 items-center">
+        <ul className="flex justify-start gap-7 max-sm:gap-2 max-sm:ml-2 mr-auto ml-7 px-2 max-sm:text-sm text-white text-xl ">
+          <li><a href="http://127.0.0.1:3000" className='no-underline  hover:text-gray-500'>Home</a></li>
+          <li><a href="http://127.0.0.1:3000" className='no-underline  hover:text-gray-500'>About</a></li>
+          <li><a href="http://127.0.0.1:3000" className='no-underline  hover:text-gray-500'>Contact</a></li>
+          <li><a href="http://127.0.0.1:3000" className='no-underline  hover:text-gray-500'>Portfolio</a></li>
         </ul>
-        <img src={image} alt="image1" className="max-sm:size-7 ml-auto mr-2 md:size-9 hover:animate-spin"></img>
+        <img src={image} alt="image1" className="max-sm:size-7 ml-auto mr-2 size-9 hover:animate-spin"></img>
       </nav>
 
     </>
